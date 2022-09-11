e02f6788c266d0b8bc45754e984ab6a224291d12
Strega's Gate <55062873+STREGAsGate@users.noreply.github.com>
2022-09-11 13:53:04 -0400

    Make Lua object managed or unmanaged

--- Sources/Lua/Lua.swift
+++ Sources/Lua/Lua.swift
@@ -10,27 +10,39 @@ import _LuaC
 
 public class Lua {
     @usableFromInline let state: OpaquePointer
-    
+    private let managed: Bool
     /**
      - description: Creates a new Lua state. It calls lua_newstate with an allocator based on the standard C allocation functions and then sets a warning function and a panic function (see §4.4) that print messages to the standard error output.
      - returns:Returns the new state, or NULL if there is a memory allocation error.
      **/
     public convenience init?() {
         guard let state = luaL_newstate() else {return nil}
-        self.init(state)
+        self.init(managedState: state)
     }
     
     public convenience init?(_ f: @escaping Alloc, _ ud: UnsafeMutableRawPointer!) {
         guard let state = lua_newstate(f, ud) else {return nil}
-        self.init(state)
+        self.init(managedState: state)
     }
     
     @usableFromInline
-    internal init(_ state: OpaquePointer) {
+    internal init(managedState state: OpaquePointer) {
         self.state = state
+        managed = true
+    }
+    
+    /// Use this in CFunctions to gain access `let lua = Lua(state)`
+    public init(existingState state: OpaquePointer) {
+        self.state = state
+        managed = false
     }
     
     deinit {
-        self.close()
+        if managed {
+            self.close()
+        }
+    }
+}
+
     }
 }
